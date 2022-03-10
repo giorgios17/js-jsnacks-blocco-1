f3600722d9f06a6fe1e63565b174b7c8a721607b
giorgios17 <giosur@live.it>
2022-03-10 16:16:00 +0100

    lista invitati

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,8 +1,16 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 // chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
-const invitati = ['giorgio', 'giuseppe', 'giovanni', 'paolo', 'pietro', 'federico']
-document.querySelector('.invitati').innerHTML = invitati;
+const invitati = ['giorgio', 'giuseppe', 'giovanni', 'paolo', 'pietro', 'federico', 'elena', 'valentina', 'serena', 'raffaele', 'anna']
+
+let invitatiTotali = ''
+
+for (let i = 0; i < invitati.length; i++) {
+    invitatiTotali += '-' + invitati[i] + ' '
+}
+
+document.querySelector('.invitati').innerHTML = invitatiTotali;
+
 
 let nomeUtente = prompt('Come ti chiami?');
 
@@ -21,7 +29,7 @@ for (let i = 0; i < invitati.length && controlloInvitato === false; i++) {
 
 if (controlloInvitato === false) {
     console.log('Mi spiace, non sei invitato alla festa.');
-    document.getElementById('controllo').innerHTML = 'Mi spiace ' + nomeUtente + ', non sei invitato alla festa.';
+    document.getElementById('controllo').innerHTML = 'Mi spiace ' + nomeUtente + ', non sei nella lista degli invitati.';
 
 
 }

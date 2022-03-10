7f37cb8b56a01e9f93bc7faec72b519d9b64976a
giorgios17 <giosur@live.it>
2022-03-10 15:27:34 +0100

    skin

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -2,21 +2,26 @@
 // chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 const invitati = ['giorgio', 'giuseppe', 'giovanni', 'paolo', 'pietro', 'federico']
+document.querySelector('.invitati').innerHTML = invitati;
 
-const nomeUtente = prompt('Come ti chiami?')
+let nomeUtente = prompt('Come ti chiami?');
 
 let controlloInvitato = false;
 
+
 for (let i = 0; i < invitati.length && controlloInvitato === false; i++) {
     if (nomeUtente.trim().toLowerCase() === invitati[i]) {
-        console.log('Entra pure alla festa e buon divertimento!')
+        console.log('Entra pure alla festa e buon divertimento!');
         controlloInvitato = true;
+        document.getElementById('controllo').innerHTML = 'Entra pure alla festa e buon divertimento!';
     }
 
 
 }
 
 if (controlloInvitato === false) {
-    console.log('Mi spiace, non sei invitato alla festa.')
+    console.log('Mi spiace, non sei invitato alla festa.');
+    document.getElementById('controllo').innerHTML = 'Mi spiace ' + nomeUtente + ', non sei invitato alla festa.';
+
 
 }
